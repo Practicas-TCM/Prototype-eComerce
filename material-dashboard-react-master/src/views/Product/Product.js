9808f4ce084f530dffbedfdc1d81dc915375a749
judith-barberan <judithbarmar2@gmail.com>
2020-05-03 20:59:34 +0200

    falta acabar taula + detalls

--- material-dashboard-react-master/src/views/Product/Product.js
+++ material-dashboard-react-master/src/views/Product/Product.js
@@ -4,6 +4,8 @@ import Button from "components/CustomButtons/Button.js";
 
 import InputGroup from 'react-bootstrap/InputGroup'
 
+import Table from "components/Table/Table.js";
+
 import Icon from "@material-ui/core/Icon";
 
 import GridItem from "components/Grid/GridItem.js";
@@ -136,37 +138,42 @@ export default function UserProfile() {
     <div>
       <GridContainer fluid>
         <GridItem /*style={{width:"50%"}}*/ xs={12} sm={12} md={5}>
+          {/* CARD 1 */}
           <Card>
+            {/* IMATGE PRODUCTE  */}
             <div style={{ textAlign: "center" }}>
-              {/* CAL CENTRAR LA IMATGE!!!  */}
               <img style={{ width: "70%" }} src={producte} alt="fence product" />
             </div>
           </Card>
         </GridItem>
         <GridItem /*style={{width:"50%"}}*/ xs={12} sm={12} md={5}>
+          {/*CARD 2*/}
           <Card>
             <div style={{ margin: "20px" }}>
               <div>
                 <h3 style={{ fontWeight: "bold", textAlign: "center", marginBottom: "30px" }}>Valla de fusta classica 120 x 210 {"\n"}</h3>
+
+                {/*ZONA PREUS*/}
                 <div>
                   <span style={{ color: "red", fontSize: "xxx-large" }}>30,99€ </span>
                   <span style={{ color: "grey" }}>SENSE IVA: 25,67€</span><br></br><br></br>
                 </div>
               </div>
 
+              {/*ZONA DETALLS PRINCIPALS*/}
               <div style={{ fontWeight: "bold" }}>
-
                 <span>Enviament:</span> <span style={{ color: "red" }}> 15,50€</span><br></br>
-                <span>Marca:</span> <br></br>
+                <span>Marca: JARBRIC</span> <br></br>
                 <span>Models disponibles:</span> <br></br>
                 <span style={{ float: "left", }}>Quantitat: </span>
                 <span style={{ float: "left", cursor: "pointer" }} class="material-icons" onClick={() => addOneProduct()}>add_box</span>
-                <div style={{ float: "left", marginLeft:"10px", marginRight:"10px"}}>{qtt}</div>
+                <div style={{ float: "left", marginLeft: "10px", marginRight: "10px" }}>{qtt}</div>
                 <span style={{ cursor: "pointer" }} class="material-icons" onClick={() => minusOneProduct()}>indeterminate_check_box</span>
 
               </div>
-              <div style={{ textAlign: "center" }}>
 
+              {/*ZONA BOTONS*/}
+              <div style={{ textAlign: "center" }}>
                 <Button justIcon round color="danger" onClick={() => showNotification()}><Favorite /></Button>
                 <Snackbar
                   place={place}
@@ -183,6 +190,27 @@ export default function UserProfile() {
                 <Button color="warning" round><span class="material-icons">done</span> Comprar </Button>
               </div>
 
+            </div>
+          </Card>
+
+        </GridItem>
+        <GridItem /*style={{width:"50%"}}*/ xs={12} sm={12} md={10}>
+          {/* CARD 3 */}
+          <Card>
+            {/* TAULA DETALLS DEL PRODUCTE  */}
+            <div style={{ textAlign: "center" }}>
+            <Table
+                tableHeaderColor="primary"
+                
+                tableData={[
+                  ["Dakota Rice", "Niger"],
+                  ["Minerva Hooper", "Curaçao"],
+                  ["Sage Rodriguez", "Netherlands"],
+                  ["Philip Chaney", "Korea, South"],
+                  ["Doris Greene", "Malawi"],
+                  ["Mason Porter", "Chile"]
+                ]}
+              />
             </div>
           </Card>
         </GridItem>
@@ -218,4 +246,4 @@ export default function UserProfile() {
 
 
 
-
+//https://www.manomano.es/p/valla-madera-clasica-lama-rayada-11959666?model_id=11959669
