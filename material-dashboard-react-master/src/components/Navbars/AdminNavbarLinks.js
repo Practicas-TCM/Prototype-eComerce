65867b6b20be84441a0c28ecc4988529ca61e53d
MeritxellLlop <mllop@edu.tecnocampus.cat>
2020-05-11 11:06:08 +0200

    Canvis fets

--- material-dashboard-react-master/src/components/Navbars/AdminNavbarLinks.js
+++ material-dashboard-react-master/src/components/Navbars/AdminNavbarLinks.js
@@ -76,7 +76,7 @@ export default function AdminNavbarLinks() {
           className={classes.buttonLink}
         >
           <Notifications className={classes.icons} />
-          <span className={classes.notifications}>5</span>
+          <span className={classes.notifications}>4</span>
           <Hidden mdUp implementation="css">
             <p onClick={handleCloseNotification} className={classes.linkText}>
               Notification
@@ -110,32 +110,27 @@ export default function AdminNavbarLinks() {
                       onClick={handleCloseNotification}
                       className={classes.dropdownItem}
                     >
-                      Mike John responded to your email
+                      Productes recomenats per a tu
                     </MenuItem>
                     <MenuItem
                       onClick={handleCloseNotification}
                       className={classes.dropdownItem}
                     >
-                      You have 5 new tasks
+                      Ofertes del mes
                     </MenuItem>
                     <MenuItem
                       onClick={handleCloseNotification}
                       className={classes.dropdownItem}
                     >
-                      You{"'"}re now friend with Andrew
+                      Els més venuts
                     </MenuItem>
                     <MenuItem
                       onClick={handleCloseNotification}
                       className={classes.dropdownItem}
                     >
-                      Another Notification
+                      Nous productes
                     </MenuItem>
-                    <MenuItem
-                      onClick={handleCloseNotification}
-                      className={classes.dropdownItem}
-                    >
-                      Another One
-                    </MenuItem>
+
                   </MenuList>
                 </ClickAwayListener>
               </Paper>
