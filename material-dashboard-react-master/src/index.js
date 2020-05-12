c455eb0f7e0ce81adccbdd63836f3c50753275c0
judith-barberan <judithbarmar2@gmail.com>
2020-05-12 11:13:19 +0200

    inici pantalla correcte

--- material-dashboard-react-master/src/index.js
+++ material-dashboard-react-master/src/index.js
@@ -31,7 +31,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/table" />
+      <Redirect from="/" to="/admin/home" />
     </Switch>
   </Router>,
   document.getElementById("root")
