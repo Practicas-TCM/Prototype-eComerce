98f7df7818fc2616c66fbe29a13ed6ae23409561
judith-barberan <judithbarmar2@gmail.com>
2020-06-09 19:40:47 +0200

    faltes ortografia

--- material-dashboard-react-master/src/views/Company/Company.js
+++ material-dashboard-react-master/src/views/Company/Company.js
@@ -22,29 +22,29 @@ export default function Company() {
                     </ul>
                 </li>
                 <li type="square"><span style={{ fontWeight: "bolder" }}>ENUNCIAT: </span> <br></br>
-                    <p>El projecte prototype té com objectiu potenciar l’autoaprenentatge i la possibilitat d’aprendre noves tecnologies que possiblement no es poden ensenyar a la carrera, per falta de temps o per ser molt específiques.
+                    <p>El projecte prototype té com a objectiu potenciar l’autoaprenentatge i la possibilitat d’aprendre noves tecnologies que possiblement no es poden ensenyar a la carrera, per falta de temps o per ser molt específiques.
                     </p>
                     <p>Es tracta d’un petit projecte de temàtica oberta que es realitzarà amb el mateix grup de pràctiques que el projecte en grup.
                     </p>
                     <p>Cada grup ha d’escollir una tecnologia i el seu repte. Disposarà d’un mes per fer un petit prototip amb aquesta tecnologia i l’haurà de presentar a la classe fent una petita demostració. Es recomana utilitzar tecnologia no treballada fins ara a classe.
                     </p>
                 </li>
-                <li type="square"><span style={{ fontWeight: "bolder" }}>PROPOSTA: </span> <span>eComerce</span> <br></br>
+                <li type="square"><span style={{ fontWeight: "bolder" }}>PROPOSTA: </span> <span>eCommerce</span> <br></br>
                     <span>En aquest projecte, ens hem proposat el repte de dissenyar i desenvolupar una botiga online de bricolatge anomenada bricoCampus.com .</span>
                     <span>La nostra botiga online consta de:</span>
                     <ul>
                         <li type="circle">Una pàgina principal amb diverses imatges d'alguns productes.</li>
                         <li type="circle">Una pàgina que mostra els productes</li>
-                        <li type="circle">Cada producte té una pàgina amb la seva descripció i caracteristiques</li>
+                        <li type="circle">Cada producte té una pàgina amb la seva descripció i característiques</li>
                         <li type="circle">Una pàgina que mostra els productes que es vol comprar</li>
                         <li type="circle">Una pàgina per realitzar el pagament</li>
-                        <li type="circle">Una pagina per registrar-se</li>
+                        <li type="circle">Una pàgina per registrar-se</li>
                         <li type="circle">Una pàgina per accedir al teu perfil d'usuari</li>
-                        <li type="circle">Una pàgina per a que el usuari pugui fer-se prèmium.</li>
+                        <li type="circle">Una pàgina perquè l'usuari pugui fer-se prèmium.</li>
                         <li type="circle">Una pàgina amb les ofertes del mes.</li>
                         <li type="circle">Una pàgina amb els més venuts.</li>
                         <li type="circle">Una pàgina amb els nous productes de la botiga.</li>
-                        <li type="circle">Una pàgina on s'explica qui sóm i que és aquest projecte.</li>
+                        <li type="circle">Una pàgina on s'explica qui som i que és aquest projecte.</li>
                     </ul>                   
                 </li>
             </ul>
